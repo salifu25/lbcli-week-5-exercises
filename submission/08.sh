ad29443058eb2b183f0c7703764cd98bb9ad407b
Salifu Yakubu <salifuyakubu333@gmail.com>
2026-07-22 15:29:47 +0000

    Update 08.sh

--- submission/08.sh
+++ submission/08.sh
@@ -2,7 +2,16 @@
 # Time-based CSV uses 512-second units with the type flag (bit 22) set
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
-seconds=$((180*24*3600))
-units=$((seconds/512))
-value=$((units | (1<<22)))
-btcc $value OP_CHECKSEQUENCEVERIFY OP_DROP $publicKey OP_CHECKSIG
+
+seconds=$((6 * 30 * 24 * 60 * 60))
+units=$((seconds / 512))
+value=$((units | (1 << 22)))
+
+lehex=$(printf '%02x%02x%02x' $((value & 0xff)) $(((value >> 8) & 0xff)) $(((value >> 16) & 0xff)))
+push=$(printf '%02x' $((${#lehex} / 2)))
+
+descriptor=$(bitcoin-cli -regtest getdescriptorinfo "pkh($publicKey)" | jq -r '.descriptor')
+address=$(bitcoin-cli -regtest deriveaddresses "$descriptor" | jq -r '.[0]')
+p2pkh=$(bitcoin-cli -regtest validateaddress "$address" | jq -r '.scriptPubKey')
+
+echo "${push}${lehex}b275${p2pkh}"
