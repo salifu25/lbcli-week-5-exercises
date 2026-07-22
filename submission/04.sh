513369d3ceee8d4cf1dae12a13859741ada1213e
Salifu Yakubu <salifuyakubu333@gmail.com>
2026-07-22 15:19:52 +0000

    Update 04.sh

--- submission/04.sh
+++ submission/04.sh
@@ -2,4 +2,12 @@
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 timestamp=1495584032
-btcc $timestamp OP_CHECKLOCKTIMEVERIFY OP_DROP $publicKey OP_CHECKSIG
+
+lehex=$(printf '%02x%02x%02x%02x' $((timestamp & 0xff)) $(((timestamp >> 8) & 0xff)) $(((timestamp >> 16) & 0xff)) $(((timestamp >> 24) & 0xff)))
+push=$(printf '%02x' $((${#lehex} / 2)))
+
+descriptor=$(bitcoin-cli -regtest getdescriptorinfo "pkh($publicKey)" | jq -r '.descriptor')
+address=$(bitcoin-cli -regtest deriveaddresses "$descriptor" | jq -r '.[0]')
+p2pkh=$(bitcoin-cli -regtest validateaddress "$address" | jq -r '.scriptPubKey')
+
+echo "${push}${lehex}b175${p2pkh}"
